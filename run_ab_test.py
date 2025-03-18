f64e4e426d786ca90b8e8af744b2bc2c8ed371c1
Vlad Ogir <v.v.ogir@outlook.com>
2025-03-18 21:04:58 +0000

    add command line arguments to run_ab_test.py for customizable benchmarking requests

--- run_ab_test.py
+++ run_ab_test.py
@@ -1,10 +1,17 @@
 import os
+import argparse
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Run Apache Benchmark (ab) test and get average latency from Memcached.')
+parser.add_argument('-n', type=int, default=1000, help='Number of requests to perform for the benchmarking session')
+parser.add_argument('-c', type=int, default=10, help='Number of multiple requests to perform at a time')
+args = parser.parse_args()
 
 # Step 1: Reset Memcached statistics
 os.system('python3 reset_memcached_stats.py')
 
 # Step 2: Run Apache Benchmark (ab) test
-ab_command = 'ab -n 1000 -c 10 http://localhost:8000/api/cached-data/?format=json'
+ab_command = f'ab -n {args.n} -c {args.c} http://localhost:8000/api/cached-data/?format=json'
 os.system(ab_command)
 
 # Step 3: Get average latency from Memcached
